d30a441d1afa87307a185934b8e960a555666dd6
Thomas Denis <contact@subskill.com>
2018-11-07 18:24:04 +0100

    inital commit

---
+++ app.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    document.getElementById('sky').classList.add('active-sky');
+    document.getElementById('mountain').classList.add('active-mtn');
+    document.getElementById('title').classList.add('title-active');
+};
