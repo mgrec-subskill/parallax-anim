55574519669627a6b7b501c0b690bdd1589a6f79
Thomas Denis <contact@subskill.com>
2018-11-07 19:12:27 +0100

    fix parallax

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 window.onload = function() {
     document.getElementById('sky').classList.add('active-sky');
     document.getElementById('mountain').classList.add('active-mtn');
+    document.getElementById('mountain-reverse').classList.add('active-mtn-reverse');
     document.getElementById('title').classList.add('title-active');
 };
